b3279d3d4df23cc405e13a74f385cda91157065a
Kuba Z <jakypbekovich@gmail.com>
2020-09-23 18:08:32 -0400

    updated a03_JsonFile.class

--- src/main/java/workingWithJson/a03_JsonFile.java
+++ src/main/java/workingWithJson/a03_JsonFile.java
@@ -15,11 +15,14 @@ public class a03_JsonFile {
         System.out.println(readPropertyFromJsonFile("firstName", "basicJsonObject.json") + "\n");
 
         //NOTE: part 2 - getting an array
+
+        //Class Task: 1. Print out names of all schools 2. Print out names + global code of each school
         JSONArray jsonArray = readArrayFromJsonFile("results", "arrayJsonObject.json");
         for (Object jsonObject: jsonArray){
             JSONObject object = (JSONObject)jsonObject;
             System.out.println(object.get("name") + " | open now: " + ((JSONObject)object.get("plus_code")).get("global_code"));
         }
+
     }
 
     static String readPropertyFromJsonFile(String key, String fileName){
